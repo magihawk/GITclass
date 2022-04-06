0780c0b435abaac9c44c79e043914b2e6c8fbec0
magihawk <mageshshankari@gmail.com>
2022-04-06 19:31:31 +0530

    Modified Class1

--- src/test/java/com/sample/Class1.java
+++ src/test/java/com/sample/Class1.java
@@ -1,5 +1,18 @@
 package com.sample;
 
 public class Class1 {
+	
+	public void method1() {
+		System.out.println("1");
+
+	}
+	
+	public void method2() {
+		System.out.println("2");
+	}
+	public void method3() {
+		System.out.println("3");
+
+	}
 
 }
